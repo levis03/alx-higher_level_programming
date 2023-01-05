7d4af9d64417c6aa8e6695dcffc8e9851b4b7032
Levis <levismaina03@gmail.com>
2023-01-05 10:56:53 -0800

    Print

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
+import sys
+
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-
-    from calculator_1 import add, sub, mul, div
-    import sys
-
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <x> <operator> <z>")
         sys.exit(1)
 
-        ops = {"+": add, "-": sub, "*": mul, "/": div}
-        if sys.argv[2] not in list(ops.keys()):
+        if sys.argv[2] not in ['+', '-', '*', '/']:
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
 
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+            x, o, z = sys.argv[1:]
+
+            if o == '+':
+                func = add
+            elif o == '-':
+                func = sub
+            elif o == '*':
+                func = mul
+            else:
+                func = div
+
+            print("{} {} {} = {:d}".format(x, o, z, func(int(x), int(z))))
