ea037aae47ba060e5c5ad4b58545bcac1f69ebcd
Levis <levismaina03@gmail.com>
2023-01-10 14:32:21 -0800

    Main file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -20,6 +20,5 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 int is_palindrome(listint_t **head);
-void print_python_list_info(PyObject *p);
 
 #endif /* LISTS_H */
