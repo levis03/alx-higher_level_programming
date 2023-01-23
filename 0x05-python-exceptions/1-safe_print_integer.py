dd08dbb8ea4ff1f2c4e5b83c863932691b5f9871
Levis <levismaina03@gmail.com>
2023-01-23 03:08:04 -0800

    Integers

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 
 def safe_print_integer(value):
     try:
-        print("{:d}".format(int(value)), end='\n')
-        return True
-    except:
-        return False
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
