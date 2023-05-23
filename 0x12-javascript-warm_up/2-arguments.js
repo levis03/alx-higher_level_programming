92786c28890b32b79073beeafafb7443f10e4fa9
Levis <levismaina03@gmail.com>
2023-05-23 16:00:51 +0000

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argsLength = proceess.argv.length - 2;
+
+if (agsLength === 0) {
+  console.log("No argument");
+} else if (argsLength === 1) {
+  console.log("Argument fund");
+} else {
+  console.log("Arguments found");
+}
